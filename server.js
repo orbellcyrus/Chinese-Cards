94be28585c4add84bf5c2a59570185b5ebedb316
Cyrus Orbell <orbell.cyrus@gmail.com>
2026-05-20 12:14:02 -0600

    basic table setup

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require("./config/db");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -13,13 +14,44 @@ app.get("/", (req, res) => {
 });
 
 app.get("/dictionary", (req, res) => {
-    res.render("dictionary");
+    let sql =
+    "SELECT * FROM CharacterDictionary";
+
+    db.query(sql,(err,results)=>{
+
+        if(err) throw err;
+
+        res.render(
+            "dictionary",
+            { characters: results }
+        );
+
+    });
 });
 
 app.get("/account", (req, res) => {
     res.render("account");
 });
 
+app.post("/addCharacter",(req,res)=>{
+
+    let character = req.body.character;
+    let english = req.body.english;
+    let pronunciation= req.body.pronunciation;
+
+    let sql =
+    "INSERT INTO CharacterDictionary(chinese_character,english,pronunciation) VALUES (?,?,?)";
+
+    db.query(
+        sql,
+        [character,english,pronunciation],
+        (err,result)=>{
+        if(err) throw err;
+        console.log("Character Added");
+        res.redirect("/dictionary");
+    });
+
+});
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
