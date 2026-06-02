bd722a2b2275e8125e0703abded546de67404867
Cyrus Orbell <orbell.cyrus@gmail.com>
2026-06-01 20:36:16 -0600

    fixed and added

---
+++ import.js
@@ -0,0 +1,55 @@
+const fs =
+require("fs");
+
+const csv =
+require("csv-parser");
+
+const mysql =
+require("mysql2");
+
+const db =
+mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Howdididoit6789",
+    database:"chinese"
+});
+
+fs.createReadStream(
+    "csv/hsk1.csv"
+)
+.pipe(
+    csv()
+)
+.on(
+    "data",
+    (row)=>{
+
+        let sql =
+        `
+        INSERT INTO
+        CharacterDictionary
+        (
+            chinese_character,
+            pronunciation,
+            english
+        )
+        VALUES (?,?,?)`;
+        db.query(
+            sql,
+            [
+                row.chinese_character,
+                row.pronunciation,
+                row.english
+            ],
+            (err)=>{
+                if(err)
+                    console.log(err);
+            }
+        );
+    }
+)
+.on( "end",()=>{
+        console.log("Import complete");
+    }
+);
